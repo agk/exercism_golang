0f78be6c008d77a34ce9cfb8cd5623b818eba102
Alexey Kucheryavenko <kucheryavenko.alexey@gmail.com>
2026-06-26 21:25:12 +0300

    codereview refact

--- raindrops/raindrops.go
+++ raindrops/raindrops.go
@@ -1,32 +1,31 @@
 package raindrops
 
 import (
-    "strconv"
-    "strings"
+	"strconv"
+	"strings"
 )
 
 // table rules: divider -> sound
 var rules = []struct {
-    divider	int
-    sound	string
+	divider int
+	sound   string
 }{
-    {3, "Pling"},
-    {5, "Plang"},
-    {7, "Plong"},
+	{3, "Pling"},
+	{5, "Plang"},
+	{7, "Plong"},
 }
 
 func Convert(number int) string {
-    sb := strings.Builder{}
-	
-    for _, rule := range rules {
-        if number%rule.divider == 0 {
-        	sb.WriteString(rule.sound)
-        }
-    }
-    str := sb.String()
-    
-    if str == "" {
-    	str = strconv.Itoa(number) // If the rules don't work
-    }
-    return str
+	sb := strings.Builder{}
+
+	for _, rule := range rules {
+		if number%rule.divider == 0 {
+			sb.WriteString(rule.sound)
+		}
+	}
+
+	if sb.Len() == 0 {
+		sb.WriteString(strconv.Itoa(number)) // If the rules don't work
+	}
+	return sb.String()
 }
